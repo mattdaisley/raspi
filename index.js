485216376840c99e3fd0a32e1a20ab81d9ef2c77
Matt Daisley <mattdaisley@gmail.com>
2016-09-13 17:10:32 -0600

    log all values

--- index.js
+++ index.js
@@ -7,16 +7,24 @@ var senseHat = require('./senseHat');
 var t1 = senseHat.getTemperature();
 var t2 = senseHat.getHumidity();
 var t3 = senseHat.getPressure();
+var t4 = senseHat.getTemperatureFromHumidity();
+var t5 = senseHat.getTemperatureFromPressure();
 
-var sensors = [ t1, t2, t3 ];
+var sensors = [ t1, t2, t3, t4, t5 ];
 
 Promise.all( sensors )
   .then(results => { 
     var temperature = results[0];
     var humidity = results[1];
     var pressure = results[2];
+    var humidityTemperature = results[3];
+    var pressureTemperature = results[4];
 
-    console.log(temperature, humidity, pressure);
+    console.log(temperature);
+    console.log(humidity);
+    console.log(pressure);
+    console.log(humidityTemperature);
+    console.log(pressureTemperature);
   }, reason => {
     console.log(reason)
   }
