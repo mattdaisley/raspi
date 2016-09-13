6605205b5e146ef67bca89a0f302ef7e413239bc
Matt Daisley <mattdaisley@gmail.com>
2016-09-13 16:06:53 -0600

    fix path

--- senseHat/index.js
+++ senseHat/index.js
@@ -1,7 +1,7 @@
 var PythonShell = require('python-shell');
 
 // receive a message in JSON mode 
-var shell = new PythonShell('get_temperature.py', { mode: 'json '});
+var shell = new PythonShell('sense/get_temperature.py', { mode: 'json '});
 shell.on('message', function (message) {
   // handle message (a line of text from stdout, parsed as JSON) 
   console.log(message);
