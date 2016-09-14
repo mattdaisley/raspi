fba92b08b55a58e8c1a4494b84d96654254d6e28
Matt Daisley <mattdaisley@gmail.com>
2016-09-13 18:11:09 -0600

    down to just temp and humidity

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ var senseHat = require('./senseHat');
 
 var t1 = senseHat.getTemperature();
 var t2 = senseHat.getHumidity();
-var t3 = senseHat.getPressure();
-var t4 = senseHat.getTemperatureFromHumidity();
-var t5 = senseHat.getTemperatureFromPressure();
 
 var sensors = [ t1, t2, t3, t4, t5 ];
 
@@ -16,15 +13,9 @@ Promise.all( sensors )
   .then(results => { 
     var temperature = results[0];
     var humidity = results[1];
-    var pressure = results[2];
-    var humidityTemperature = results[3];
-    var pressureTemperature = results[4];
 
     console.log(temperature);
     console.log(humidity);
-    console.log(pressure);
-    console.log(humidityTemperature);
-    console.log(pressureTemperature);
   }, reason => {
     console.log(reason)
   }
